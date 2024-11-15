402fd3798be21d535b216c0ffc17d89750a5d335
Bolivar Alexis <alexiiz360@gmail.com>
2024-11-15 14:10:09 -0500

    get api and paint card

--- GMS/src/Pages/Home/index.jsx
+++ GMS/src/Pages/Home/index.jsx
@@ -1,9 +1,23 @@
-import Layout from '../../Components/Layout'
+import { useState, useEffect} from 'react';
+import Layout from '../../Components/Layout';
+import Card from '../../Components/Cards'
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response=>response.json())
+    .then(data=>setItems(data))
+  },[])
   return (
     <Layout>
-      home
+      HOME
+      {
+      items?.map(item =>(
+      <Card key={item.id} data={item}/>
+      ))
+      }
     </Layout>
   )
 }
