15280a30bd87e7a011161729d8134c2437400ebd
Bolivar Alexis <alexiiz360@gmail.com>
2025-01-03 19:23:55 -0500

    add state and context

--- GMS/src/Components/Navbar/index.jsx
+++ GMS/src/Components/Navbar/index.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import { NavLink } from "react-router-dom"
+import { ShoppingCartContext } from '../../Context'
+
 
 const Navbar = () => {
+    const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-4'
 
     return(
@@ -86,7 +90,7 @@ const Navbar = () => {
                     <NavLink to='/furniture'
                     className={({isActive})=>
                         isActive?activeStyle:undefined}>
-                    🛒 0
+                    🛒 {context.count}
                     </NavLink>    
                 </li>
                 <li>
