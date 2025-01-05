91af448d260379ae0b46b9cbd0aea15428891889
Bolivar Alexis <alexiiz360@gmail.com>
2025-01-05 13:40:27 -0500

    update of product detail

--- GMS/src/Components/ProductDetail/index.jsx
+++ GMS/src/Components/ProductDetail/index.jsx
@@ -3,7 +3,7 @@ import {XCircleIcon} from '@heroicons/react/24/outline'
 
 const ProductDetails = ()=>{
     return(
-        <aside className='product-detail backdrop-blur-[16px] backdrop-saturate-[100%] bg-[#0000] bg-opacity-10border border-opacity-20 border-[#ffffff] flex flex-col fixed right-0 border border-black rounded-lg'>
+        <aside className='product-detail backdrop-blur-[16px] backdrop-saturate-[100%] bg-[#0000] bg-opacity-10border border-opacity-20 border-[#ffffff] flex flex-col fixed right-2 border border-black rounded-lg p-4'>
             <div className='flex justify-between items-center p-4'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <XCircleIcon className="size-6 text-black-500" />
